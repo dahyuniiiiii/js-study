9776e9878a5e59f2f4badea0eafd7959e146b7e7
dahyun_oh <imcute0703123@naver.com>
2024-11-12 20:04:35 +0900

    Js Day6

--- For.js
+++ For.js
@@ -1,3 +1,20 @@
-for(let i=0;i<10;i++) { //i++는 i=i+1
-    console.log("안녕!!")
+for(let i=0; i<10; i+=2) { //초기식 조건식 증감식
+    console.log("안녕!!",i)
+}
+
+for(let i=2; i<=9; i++){
+    for(let j = 1; j<=9; j++){
+        console.log(i+"*"+j+"="+i*j)
+    }
+}
+
+let i = 2 //초기식
+while(i<10){ //조건식
+    console.log("while문 실행",i)
+    i++; //증감식
+}
+
+let fruit = ["banana","apple","grape","mango"]
+for(let i=0; i<fruit.length; i++){
+    console.log(fruit[i])
 }
--- IfTest.js
+++ IfTest.js
@@ -1,1 +1,40 @@
-1.
+//<문제1>
+//유저가 입력하는 숫자가 0인지 음수인지 양수인지 판단하는 프로그램을 만드시오
+let num = prompt("숫자를 입력하세요:");
+num = Number(num); // 입력값을 숫자로 변환
+if (num>0){
+    console.log("양수입니다.")
+}else if(num==0){ //else랑 if는 띄어써야함
+    console.log("0입니다.")
+}else{
+    console.log("음수입니다.")
+}
+
+//<문제2>
+//나는 대학교 교수다. 레포트 점수에 따라 등급을 매기는 프로그램을 만드시오
+if (score >= 90 && score <= 100) { //90 <= score <= 100와 같은 다중비교가 허용되지 않음
+    console.log("A등급");
+} else if (score >= 80 && score < 90) {
+    console.log("B등급");
+} else if (score >= 70 && score < 80) {
+    console.log("C등급");
+} else if (score >= 60 && score < 70) {
+    console.log("D등급");
+} else {
+    console.log("F등급");
+}
+//전체 들여쓰기 줄이는 단축키 : shift tap
+
+//<문제3>
+//한 지원자가 우리 회사에 지원을 했다. 지원자가 사용 가능한 스킬은 배열에 제공이 된다
+//let skills = ["HTML","CSS","Javascript","React"]
+//Javascript와 React 둘 다 할 줄 안다면 “합격!” Javascript와 React 둘 중 하나만 할 줄 안다면 “예비”, 두 스킬이 없다면 “탈락” 을 보여주는 프로그램을 짜시오
+let skills = ["HTML", "CSS", "Javascript", "React"];
+
+if (skills.includes("Javascript") && skills.includes("React")) {
+    console.log("합격!");
+} else if (skills.includes("Javascript") || skills.includes("React")) {
+    console.log("예비");
+} else {
+    console.log("탈락");
+}
