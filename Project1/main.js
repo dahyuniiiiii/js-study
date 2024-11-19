7deba12e0abcea4221828b8103b879dfcd384ca8
dahyun_oh <imcute0703123@naver.com>
2024-11-19 22:55:52 +0900

    Js Day10

--- Project1/main.js
+++ Project1/main.js
@@ -8,8 +8,63 @@
 //유저가 1~100 범위 밖에 숫자를 입력하면 알려준다. 기회를 깍지 않는다
 //유저가 이미 입력한 숫자를 또 입력하면, 알려준다, 기회를 깍지 않는다
 
-let computerNum = 0
-function pickRandomNum(){
-    computerNum = Math.random()
-    console.log("정답",computerNum)
+let computerNum = 0;
+let playButton = document.getElementById("play-button");
+let userInput = document.getElementById("user-input");
+let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history=[]
+
+playButton.addEventListener("click",play); //함수도 매개변수로 보낼 수 있음
+resetButton.addEventListener("click",reset);
+userInput.addEventListener("focus",function(){userInput.value="";});
+
+function pickRandomNum(){ //랜덤번호 뽑기
+    computerNum = Math.floor(Math.random() * 100)+1; //Math.floor은 소숫점 버림 함수, 1~100까지 랜덤함수
+    console.log("정답",computerNum);
 }
+
+function play(){ 
+    let userValue = userInput.value;
+
+    if(userValue<1 || userValue>100){
+        resultArea.textContent="1과 100사이 숫자를 입력해주세요"
+        return;
+    }
+
+    if(history.includes(userValue)){
+        resultArea.textContent="이미 입력한 숫자입니다 다른 숫자를 입력해주세요";
+        return;
+    }
+    chances -- ;
+    chanceArea.textContent=`남은기회:${chances}번`;
+
+    if (userValue < computerNum){
+        resultArea.textContent ="Up!!!";
+    }else if(userValue > computerNum){
+        resultArea.textContent = "Down!!!";
+    }else{
+        resultArea.textContent = "맞췄습니다!!!";
+    }
+
+    history.push(userValue)
+
+    if(chances < 1){
+        gameOver = true
+    }
+
+    if(gameOver == true){
+        playButton.disabled = true;
+    }
+}
+function reset(){
+    //user input창이 깨끗하게 정리되고 새로운 번호가 생성
+    userInput.value = "";
+    pickRandomNum();
+
+    resultArea.textContent="결과값이 여기 나옵니다!";
+}
+pickRandomNum(); //호출
