d34ca29c8139bc262f0565c999d12c5a19a85f84
dahyun_oh <imcute0703123@naver.com>
2024-11-29 19:31:13 +0900

    Js Day13

--- Es6.js
+++ Es6.js
@@ -1,1 +1,17 @@
-let 
+//객체 만들기
+let name = "dahyun"
+let age = 23
+let person = {name,age}
+console.log(person)
+
+//객체 분해하기
+let bts = {
+    name:"방탄소년단",
+    num:7
+}
+let name = bts.name
+let num = bts.num
+
+let {name,num} = bts
+
+console.log(name,num)
