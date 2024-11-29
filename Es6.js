0fffa544ea5923ed50d2b0f64afdaecc7ee10687
dahyun_oh <imcute0703123@naver.com>
2024-11-30 00:36:53 +0900

    Js Day14

--- Es6.js
+++ Es6.js
@@ -1,17 +1,24 @@
 //객체 만들기
 let name = "dahyun"
 let age = 23
-let person = {name,age}
-console.log(person)
+console.log(`제이름은 ${name}입니다 제 나이는 ${age}입니다`)
 
 //객체 분해하기
 let bts = {
     name:"방탄소년단",
     num:7
 }
-let name = bts.name
-let num = bts.num
-
 let {name,num} = bts
 
-console.log(name,num)
+//배열
+let array =[1,2,3]
+let [a,...rest] = array
+console.log(rest) //a를 뺀 나머지가 rest에 들어감
+console.log(a, rest)
+
+//이어붙이기
+let a = [1,2]
+let b = [3,4]
+let c = [5,6]
+let result = a.concat(b,c) //= let result = [...a,...b,...c]
+console.log(result)
